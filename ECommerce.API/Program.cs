adda59643785048646485aabc9bc30aa6bb5af78
ahmet <ahmet@DESKTOP-6GV7GM5>
2025-05-30 01:24:12 +0300

    DI registerlar programcs eklendi.

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,10 +1,12 @@
 using ECommerce.API.Middleware;
 using ECommerce.Application.Interfaces;
 using ECommerce.Application.Mappings;
+using ECommerce.Application.Services;
 using ECommerce.Domain.Interfaces;
 using ECommerce.Infrastructure.Configurations;
 using ECommerce.Infrastructure.Persistences;
 using ECommerce.Infrastructure.Policies;
+using ECommerce.Infrastructure.Repositories;
 using ECommerce.Infrastructure.Services;
 using ECommerce.Infrastructure.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
@@ -32,13 +34,17 @@ builder.Services.AddHttpClient<IClientBalanceService, ClientBalanceService>().Ad
 builder.Services.AddDbContext<OrderDbContext>(options => options.UseInMemoryDatabase("OrderDb"));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IBalanceService, BalanceService>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IBalanceRepository, BalanceRepository>();
+
 
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseSwagger();
-app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
-
