b7f987092c942e936736fa7874e0840db31fa712
ahmet <ahmet@DESKTOP-6GV7GM5>
2025-05-30 01:31:26 +0300

    MappingProfile mapler eklendi.

--- ECommerce.Application/Mappings/MappingProfile.cs
+++ ECommerce.Application/Mappings/MappingProfile.cs
@@ -1,10 +1,14 @@
 ﻿using AutoMapper;
+using ECommerce.Application.DTOs;
+using ECommerce.Application.DTOs.BalanceManagement.Order;
+using ECommerce.Domain.Entities;
 
 namespace ECommerce.Application.Mappings;
 public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-
+        CreateMap<Balance, BalanceDto>().ReverseMap();
+        CreateMap<OrderHeader, OrderHeaderDto>().ReverseMap();
     }
 }
